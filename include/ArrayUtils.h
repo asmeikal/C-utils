1dd6964ab9e3b9ec1443af3f0335955208e35761
Michele Laurenti <asmeikal@me.com>
2016-01-02 20:38:17 +0100

    Add general headers

---
+++ include/ArrayUtils.h
@@ -0,0 +1,17 @@
+//
+//  utils.h
+//  openCL
+//
+//  Created by Michele Laurenti on 19/12/15.
+//
+//
+
+#ifndef utils_h
+#define utils_h
+
+#define ARRAY_LEN(x)	(sizeof((x))/sizeof((x)[0]))
+
+
+
+#endif /* utils_h */
+
---
+++ include/MLUtils.h
@@ -0,0 +1,7 @@
+#ifndef __ML_UTILS
+#define __ML_UTILS
+
+#define UNUSED(x)	(void)(x)
+
+#endif
+
